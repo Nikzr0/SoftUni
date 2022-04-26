b5318fd2cc1ce9de5152c438211128f8f92f737c
Nikzr0 <npepelov@gmail.com>
2022-04-27 00:01:25 +0300

    list exercise one

---
+++ List/List/1. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();// 3 3 6 1
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            int sum = 0;
+            if (i >= 1)
+            {
+                if (list[i - 1] == list[i])
+                {
+                    sum = list[i - 1] + list[i];
+                    list[i - 1] = sum;
+                    list.RemoveAt(i);
+                    i--;
+                    i = i - 1;
+                }
+            }
+        }
+
+        Console.WriteLine(String.Join(" ", list));
+    }
+}
