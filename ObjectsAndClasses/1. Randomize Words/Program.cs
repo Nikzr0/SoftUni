a0515534b807fd54cc40652ba306fda242f6fa38
Nikzr0 <npepelov@gmail.com>
2022-05-09 23:24:27 +0300

    objects - exercise 1

---
+++ ObjectsAndClasses/1. Randomize Words/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] arrays = Console.ReadLine().Split();
+        Random random = new Random();
+
+        for (int i = 0; i < arrays.Length; i++)// 1 2 3 4
+        {
+            int newposition = random.Next(arrays.Length);
+            string temp = arrays[i];// 1
+            arrays[i] = arrays[newposition];// 3
+            arrays[newposition] = temp;
+        }
+
+        Console.WriteLine(string.Join("\n",arrays));
+    }
+}
