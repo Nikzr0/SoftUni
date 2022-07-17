6b0bdf2d1b220a39a2e4084606e808d8772975d2
Nikzr0 <npepelov@gmail.com>
2022-07-17 18:03:52 +0300

    Associative Arrays: Exercises (ex 1-2)

---
+++ Associative Arrays/Associative Arrays_Exercises/2. A Miner Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> resourses = new Dictionary<string, int>();
+
+        while (true)
+        {
+            string resourse = Console.ReadLine();
+
+            if (resourse == "stop")
+            {
+                break;
+            }
+
+            int quantity = int.Parse(Console.ReadLine());
+
+            if (!resourses.ContainsKey(resourse))
+            {
+                resourses.Add(resourse, quantity);
+            }
+            else
+            {
+                resourses[resourse] += quantity;
+            }
+        }
+
+        foreach (var item in resourses)
+        {
+            Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+}
---
+++ Associative Arrays/Associative Arrays_Exercises/Associative Arrays_ Exercises/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> wordsLetters = new Dictionary<string, int>();
+
+        string input = Console.ReadLine();
+
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input.Substring(i,1) != " ")
+            {
+                if (!wordsLetters.ContainsKey(input.Substring(i,1)))
+                {
+                    wordsLetters.Add(input.Substring(i, 1), 1);
+                }
+                else
+                {
+                    wordsLetters[input.Substring(i, 1)]++;
+                }
+
+            }
+        }
+
+        foreach (var item in wordsLetters)
+        {
+            Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+}
