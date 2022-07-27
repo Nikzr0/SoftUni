0c115f762f35943657f6528f1d9866d33d8543dd
Nikzr0 <npepelov@gmail.com>
2022-07-28 00:13:42 +0300

    RegularExpressions (ex 5 - not working (only zero tests))

--- Fundamentals/09.RegularExpressions/Lab/Ex05.NetherRealms/Program.cs
+++ Fundamentals/09.RegularExpressions/Lab/Ex05.NetherRealms/Program.cs
@@ -22,86 +22,116 @@ namespace Ex05.NetherRealms
             int numberOfStars = 0;
             int starsHelp = 2;
 
+            int numberOfSlashes = 0;
+            int slashesHelp = 2;
+
 
             for (int i = 0; i < input.Length; i++)
             {
-                MatchCollection myMatch = healthRegex.Matches(input[i]);
-                MatchCollection damageMatch = damage.Matches(input[i]);
-
-                foreach (Match item in myMatch)
+                string name = input[i];
+                for (int z = 0; z < input.Length; z++)
                 {
-                    healthPoints += item.ToString();
-                }
 
-                foreach (var item in healthPoints)
-                {
-                    healthPointsSum += (int)item;
-                }
-
-                string starsCounter = input[i];
-
-
-                for (int y = 0; y < starsCounter.Length; y++)
-                {
-                    if (starsCounter[y].ToString() == "*")
+                    if (name.Length > 0 && !name.Contains(',') && !name.Contains(' '))
                     {
-                        numberOfStars++;
+                        MatchCollection myMatch = healthRegex.Matches(input[i]);
+                        MatchCollection damageMatch = damage.Matches(input[i]);
+
+                        foreach (Match item in myMatch)
+                        {
+                            healthPoints += item.ToString();
+                        }
+
+                        foreach (var item in healthPoints)
+                        {
+                            healthPointsSum += (int)item;
+                        }
+
+                        string starsCounter = input[i];
+
+
+                        for (int y = 0; y < starsCounter.Length; y++)
+                        {
+                            if (starsCounter[y].ToString() == "*")
+                            {
+                                numberOfStars++;
+                            }
+
+                            if (starsCounter[y].ToString() == "/")
+                            {
+                                numberOfSlashes++;
+                            }
+                        }
+
+                        for (int x = 0; x < numberOfStars - 1; x++)
+                        {
+                            starsHelp = starsHelp * 2;
+                        }
+
+                        for (int x = 0; x < numberOfSlashes - 1; x++)
+                        {
+                            starsHelp = starsHelp * 2;
+                        }
+
+                        foreach (Match item in damageMatch)
+                        {
+                            string temp = item.ToString();
+
+
+                            if (temp[i].ToString() == "-")
+                            {
+                                temp = temp.Remove(0, 1);
+                                damageSum = damageSum - double.Parse(temp);
+                            }
+                            else if (temp[i].ToString() == "+")
+                            {
+                                temp = temp.Remove(0, 1);
+                                damageSum += double.Parse(temp);
+                            }
+                            else
+                            {
+                                damageSum += double.Parse(temp);
+                            }
+                        }
+
+                        if (numberOfStars > 0)
+                        {
+                            damageSum = damageSum * starsHelp;
+                        }
+
+                        if (numberOfSlashes > 0)
+                        {
+                            damageSum = damageSum / slashesHelp;
+                        }
+
+                        if (!dic.ContainsKey(input[i]))
+                        {
+                            dic.Add(input[i], new Dictionary<double, double>());
+                            dic[input[i]].Add(healthPointsSum, damageSum);
+
+                        }
+
+                        healthPoints = "";
+                        healthPointsSum = 0;
+                        damageSum = 0;
+
+                        numberOfStars = 0;
+                        starsHelp = 2;
+
+                        numberOfSlashes = 0;
+                        slashesHelp = 2;
+
                     }
                 }
-
-                for (int x = 0; x < numberOfStars - 1; x++)
-                {
-                    starsHelp = starsHelp * 2;
-                }
-
-
-
-                foreach (Match item in damageMatch)
-                {
-                    string temp = item.ToString();
-
-
-                    if (temp[i].ToString() == "-")
-                    {
-                        temp = temp.Remove(0, 1);
-                        damageSum = damageSum - double.Parse(temp);
-                    }
-                    else
-                    {
-                        damageSum += double.Parse(temp);
-                    }
-
-                }
-
-                if (numberOfStars > 0)
-                {
-                    damageSum = damageSum * starsHelp;
-                }
-
-                if (!dic.ContainsKey(input[i]))
-                {
-                    dic.Add(input[i], new Dictionary<double, double>());
-                    dic[input[i]].Add(healthPointsSum, damageSum);
-
-                }
-
-                healthPoints = "";
-                healthPointsSum = 0;
-                damageSum = 0;
-
-                numberOfStars = 0;
-                starsHelp = 2;
-
             }
 
-            foreach (var item in dic.OrderBy(x =>x.Key))
+            foreach (var item in dic.OrderBy(x => x.Key))
             {
                 foreach (var item2 in item.Value)
                 {
                     Console.WriteLine($"{item.Key} - {item2.Key} health, {item2.Value:f2} damage");
                 }
             }
-
         }
     }
 }
