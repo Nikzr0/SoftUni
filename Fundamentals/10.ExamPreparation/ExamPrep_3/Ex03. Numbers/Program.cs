db683504ee9eb280f5279b13b0d0e61645aecc98
Nikzr0 <npepelov@gmail.com>
2022-07-30 11:50:18 +0300

    ExamPrep_3 ex 3

--- Fundamentals/10.ExamPreparation/ExamPrep_3/Ex03. Numbers/Program.cs
+++ Fundamentals/10.ExamPreparation/ExamPrep_3/Ex03. Numbers/Program.cs
@@ -1,13 +1,38 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ex03._Numbers
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            List<double> numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToList();
+
+            double sum = 0;
+            double avarageSum = 0;
+
+
+            foreach (var item in numbers)
+            {
+                sum += item;
+            }
+
+            avarageSum = sum / numbers.Count;
+
+            foreach (var item in numbers.OrderByDescending(x => x).Take(1))
+            {
+                if (item <= avarageSum)
+                {
+                    Console.WriteLine("No");
+                }
+            }
+
+            foreach (var item in numbers.OrderByDescending(x => x).Where(x => x > avarageSum).Take(5))
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
