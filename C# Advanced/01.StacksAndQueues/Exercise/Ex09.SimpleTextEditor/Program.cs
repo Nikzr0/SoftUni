319b0850c5ec351c551e129c07c4aee46f258ab9
Nikzr0 <npepelov@gmail.com>
2022-08-02 19:22:10 +0300

    StacksAndQueues:Exercises (ex 9 and 10)

--- C# Advanced/01.StacksAndQueues/Exercise/Ex09.SimpleTextEditor/Program.cs
+++ C# Advanced/01.StacksAndQueues/Exercise/Ex09.SimpleTextEditor/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Ex09.SimpleTextEditor
 {
@@ -8,7 +7,98 @@ namespace Ex09.SimpleTextEditor
     {
         public static void Main()
         {
+            int numberOfOperations = int.Parse(Console.ReadLine());
 
+            Stack<string> elements = new Stack<string>();
+
+            Stack<string> stackOfCommands = new Stack<string>();
+            Stack<string> commandValue = new Stack<string>();
+
+            while (numberOfOperations > 0)
+            {
+                string[] command = Console.ReadLine().Split(" ");
+
+                switch (command[0])
+                {
+                    case "1":
+                        string strToAppend = command[1];
+
+                        string whatIsAdded = "";
+
+                        if (strToAppend.Length == 1)
+                        {
+                            elements.Push(strToAppend);
+                            whatIsAdded = strToAppend;
+                        }
+                        else
+                        {
+                            foreach (var item in strToAppend)
+                            {
+                                elements.Push(item.ToString());
+                                whatIsAdded += item.ToString();
+                            }
+                        }
+
+
+                        stackOfCommands.Push("1");
+                        commandValue.Push(whatIsAdded);
+                        break;
+
+                    case "2":
+                        string elementToRemove = command[1];
+                        int numberOfElementsToRemove = int.Parse(command[1]);
+
+                        string whatWasRemoved = "";
+                        for (int i = 0; i < numberOfElementsToRemove; i++)
+                        {
+                            whatWasRemoved += elements.Pop();
+                        }
+
+                        stackOfCommands.Push("2");
+                        commandValue.Push(whatWasRemoved);
+
+                        break;
+
+                    case "3":
+                        int positionOfElementToReturn = int.Parse(command[1]);
+
+                        int indexCounter = -1;
+                        foreach (var item in elements)
+                        {
+                            indexCounter++;
+
+                            if (elements.Count - indexCounter == positionOfElementToReturn)
+                            {
+                                Console.WriteLine(item);
+                            }
+                        }
+
+                        break;
+
+                    case "4":
+                        if (stackOfCommands.Pop() == "1")
+                        {
+                            string whatToRemove = commandValue.Pop();
+
+                            foreach (var item in whatToRemove)
+                            {
+                                elements.Pop();
+                            }
+                        }
+                        else
+                        {
+                            string whatToReturn = commandValue.Pop(); // abc
+
+                            for (int i = whatToReturn.Length - 1; i >= 0; i--)
+                            {
+                                elements.Push(whatToReturn[i].ToString());
+                            }
+                        }
+                        break;
+                }
+
+                numberOfOperations--;
+            }
         }
     }
 }
--- C# Advanced/01.StacksAndQueues/Exercise/Ex10.Crossroads/Program.cs
+++ C# Advanced/01.StacksAndQueues/Exercise/Ex10.Crossroads/Program.cs
@@ -8,7 +8,77 @@ namespace Ex10.Crossroads
     {
         public static void Main()
         {
+            int greenLightInSeconda = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
 
+            Queue<string> queueOfCars = new Queue<string>();
+
+            int savePassesCounter = 0;
+
+            List<string> listOdCrashes = new List<string>();
+
+            
+            while (true)
+            {
+                int greenLightDuration = greenLightInSeconda;
+                string nameOfCar = Console.ReadLine();
+
+                if (nameOfCar == "END")
+                {
+                    break;
+                }
+
+                if (nameOfCar.ToLower() == "green")
+                {
+                    int howMuchToRemove = 0;
+                    foreach (var item in queueOfCars)
+                    {
+                        if (greenLightDuration > 0)
+                        {
+                            if (greenLightDuration + freeWindow - item.Length >= 0)
+                            {
+                                greenLightDuration -= item.Length;
+                                howMuchToRemove++;
+                            }
+                            else
+                            {
+                                int whereWasHit = item.Length;
+
+                                whereWasHit -= greenLightDuration;
+                                whereWasHit -= freeWindow;
+
+                                string theNameOfTheCar = item;
+
+                                string hitLetter = theNameOfTheCar[item.Length - whereWasHit].ToString();
+
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{item} was hit at {hitLetter}.");
+                                System.Environment.Exit(1);
+                            }
+                            savePassesCounter++;
+                        }
+                    }
+
+                    if (howMuchToRemove > 0)
+                    {
+                        for (int i = 0; i < howMuchToRemove; i++)
+                        {
+                            queueOfCars.Dequeue();
+                        }
+                    }
+
+                    howMuchToRemove = 0;
+                }
+                else
+                {
+                    queueOfCars.Enqueue(nameOfCar);
+                }
+
+
+            }
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{savePassesCounter} total cars passed the crossroads.");
         }
     }
 }
