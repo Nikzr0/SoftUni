a6de0683b9ed337d8011b89fc35db867fd96d5f6
Nikzr0 <npepelov@gmail.com>
2022-08-03 11:13:37 +0300

    StacksAndQueues:Exercises (ex 11)

--- C# Advanced/01.StacksAndQueues/Exercise/Ex11.KeyRevolver/Program.cs
+++ C# Advanced/01.StacksAndQueues/Exercise/Ex11.KeyRevolver/Program.cs
@@ -8,6 +8,66 @@ namespace Ex11.KeyRevolver
     {
         public static void Main()
         {
+            Queue<int> queueOfLocks = new Queue<int>();
+            Stack<int> stackOfBullets = new Stack<int>();
+
+            int priceOfABullet = int.Parse(Console.ReadLine()); // 50
+            int sizeOfGunBarrel = int.Parse(Console.ReadLine());
+            int[] bullets = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] locks = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int valueOfInteligence = int.Parse(Console.ReadLine());
+
+            foreach (var item in locks)
+            {
+                queueOfLocks.Enqueue(item);
+            }
+
+            foreach (var item in bullets)
+            {
+                stackOfBullets.Push(item);
+            }
+
+
+            int reloder = sizeOfGunBarrel;
+
+            int operationLength = stackOfBullets.Count;
+
+            for (int i = 0; i < operationLength; i++)
+            {
+
+                if (reloder > 0)
+                {
+                    if (queueOfLocks.Count > 0)
+                    {
+                        if (stackOfBullets.Pop() <= queueOfLocks.Peek())
+                        {
+                            queueOfLocks.Dequeue();
+                            Console.WriteLine("Bang!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ping!");
+                        }
+                        reloder--;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Reloading");
+                    operationLength++;
+                    reloder = sizeOfGunBarrel;
+                }
+
+            }
+
+            if (stackOfBullets.Count >= 0 && queueOfLocks.Count == 0)
+            {
+                Console.WriteLine($"{stackOfBullets.Count} bullets left. Earned ${valueOfInteligence - (bullets.Length - stackOfBullets.Count) * priceOfABullet}");
+            }
+            else if (stackOfBullets.Count <= 0 && queueOfLocks.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {queueOfLocks.Count}");
+            }
 
         }
     }
