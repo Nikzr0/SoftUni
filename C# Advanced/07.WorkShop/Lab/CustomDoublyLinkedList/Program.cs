dfe92f8d10dde1118326de4f5f454235926173a9
Nikzr0 <npepelov@gmail.com>
2022-08-25 23:19:26 +0300

    WorkShop - LinkedList

---
+++ C# Advanced/07.WorkShop/Lab/CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomDoublyLinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.AddFirst(1);
+            list.AddLast(2);
+            list.AddLast(3);
+
+            list.RemoveFirst();
+            list.RemoveLast();
+            Console.WriteLine();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
