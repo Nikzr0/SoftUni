8e5a91c6ed0cea360e107d82930ca274dc5a7613
Nikzr0 <npepelov@gmail.com>
2022-10-05 23:26:57 +0300

    Lab

---
+++ C# OOP/03.InterfacesAndAbstraction/Lab/Ex01.Shapes/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+
+namespace Ex01.Shapes
+{
+    public interface IDrawable
+    {
+        string Draw();
+    }
+    public class Circle : IDrawable
+    {
+        public int Radius { get; set; }
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public string Draw() // It doesn't look like a circle
+        {
+            StringBuilder sb = new StringBuilder();
+            int gab = 2;
+
+            string bottom = new string(' ', (Radius * 2 - 2) / 2) + new string('*', gab) + new string(' ', (Radius * 2 - 2) / 2);
+            sb.AppendLine(bottom);
+            while (gab < Radius * 2)
+            {
+                string line = new string(' ', Radius - gab / 2 - 1) + '*' + new string(' ', gab) + '*' + new string(' ', Radius - gab / 2 - 1);
+                sb.AppendLine(line);
+                gab += 2;
+            }
+            gab -= 2;
+
+            while (gab >= 2)
+            {
+                string line = new string(' ', (Radius - gab / 2) - 1) + '*' + new string(' ', gab) + '*' + new string(' ', (Radius - gab / 2) - 1);
+                sb.AppendLine(line);
+                gab -= 2;
+            }
+            sb.AppendLine(bottom);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+    public class Rectangle : IDrawable
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public string Draw()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(new string('*', Width));
+            for (int i = 0; i < Height - 2; i++)
+            {
+                string b = "*" + new string(' ', Width - 2) + "*";
+                sb.AppendLine(b);
+            }
+            sb.AppendLine(new string('*', Width));
+            return sb.ToString().TrimEnd();
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height);
+
+            Console.WriteLine(circle.Draw());
+            Console.WriteLine(rect.Draw());
+        }
+    }
+}
---
+++ C# OOP/03.InterfacesAndAbstraction/Lab/Ex02.Cars/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex02.Cars
+{
+    public interface ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        string Start();
+        string Stop();
+    }
+
+    public class Seat : ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+        public string Start()
+        {
+             return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return $"Breaaal!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} Seat {Model}");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return $"Breaaal!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} {Model} with {Battery} Batteries");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+            return sb.ToString().TrimEnd();
+        }
+    }
+    public interface IElectricCar
+    {
+        public int Battery { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+
+            Console.WriteLine(seat.ToString());
+            Console.WriteLine(tesla.ToString());
+
+        }
+    }
+}
