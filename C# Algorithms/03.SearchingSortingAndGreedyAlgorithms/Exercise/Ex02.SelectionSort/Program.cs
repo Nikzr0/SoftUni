6eddfdadfcf23f79b1bd24f0edc58d1d1843d8c3
Nikzr0 <npepelov@gmail.com>
2022-10-29 14:17:39 +0300

    ex 2 and 3

---
+++ C# Algorithms/03.SearchingSortingAndGreedyAlgorithms/Exercise/Ex02.SelectionSort/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Ex02.SelectionSort
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            ArraySorter(arr, 0);
+        }
+
+        private static void ArraySorter(int[] arr, int index)
+        {
+            if (ArrValidator(arr))
+            {
+                Console.WriteLine(String.Join(" ", arr));
+                return;
+            }
+
+            int temp = arr[index];
+            int n = MinValueIndex(arr, index);
+            arr[index] = arr[n];
+            arr[n] = temp;
+
+
+            ArraySorter(arr, index + 1);
+        }
+
+        private static int MinValueIndex(int[] arr, int index)
+        {
+            int result = 0;
+            int minValue = int.MaxValue;
+
+            for (int i = index; i < arr.Length; i++)
+            {
+                if (arr[i] <  minValue)
+                {
+                    minValue = arr[i];
+                    result = i;
+                }
+            }
+
+            return result;
+        }
+        private static bool ArrValidator(int[] arr)
+        {
+            bool result = false;
+
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i - 1] && arr[i] < arr[i + 1])
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C# Algorithms/03.SearchingSortingAndGreedyAlgorithms/Exercise/Ex03.BubbleSort/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex03.BubbleSort
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            BubbleSort(arr, 0, arr.Length - 1);
+        }
+
+        private static void BubbleSort(int[] arr, int index, int endIndex)
+        {
+            if (index == arr.Length - 1 || index == endIndex)
+            {
+                endIndex--;
+                index = 0;
+            }
+            if (ArrValidator(arr))
+            {
+                Console.WriteLine(String.Join(" ", arr));
+                return;
+            }
+
+            if (arr[index] > arr[index + 1])
+            {
+                int temp = arr[index];
+                arr[index] = arr[index + 1];
+                arr[index + 1] = temp;
+            }        
+            BubbleSort(arr, index + 1, endIndex);
+        }
+
+        private static bool ArrValidator(int[] arr)
+        {
+            bool result = false;
+
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i - 1] && arr[i] < arr[i + 1])
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
