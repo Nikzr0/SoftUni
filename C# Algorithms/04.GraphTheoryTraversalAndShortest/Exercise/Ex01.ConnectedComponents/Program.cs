7f22017942ae8975ba051978bbf9eff4808595ae
Nikzr0 <90499910+Nikzr0@users.noreply.github.com>
2022-11-02 13:14:01 +0200

    ex 1-3

---
+++ C# Algorithms/04.GraphTheoryTraversalAndShortest/Exercise/Ex01.ConnectedComponents/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Ex01.ConnectedComponents
+{
+    public class Program
+    {
+        private static bool[] visited;
+        private static List<List<int>> graph;
+        static void Main()
+        {
+            graph = new List<List<int>>();
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int l = 0; l < numberOfLines; l++)
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    graph.Add(new List<int> { });
+                }
+                else
+                {
+                    List<int> lineOfNumbers = line.Split(" ").Select(int.Parse).ToList();
+                    graph.Add(lineOfNumbers);
+                }
+            }
+
+            visited = new bool[graph.Count];
+
+            for (int i = 0; i < graph.Count; i++)
+            {
+                if (visited[i])
+                {
+                    continue;
+                }
+                var output = new List<int>();
+                DFS(i, output);
+
+                Console.WriteLine($"Connected component: {String.Join(" ", output)}");
+            }
+        }
+        private static void DFS(int i, List<int> components)
+        {
+            if (visited[i])
+            {
+                return;
+            }
+
+            visited[i] = true;
+
+            foreach (var child in graph[i])
+            {
+                DFS(child, components);
+            }
+            components.Add(i);
+        }
+    }
+}
---
+++ C# Algorithms/04.GraphTheoryTraversalAndShortest/Exercise/Ex03.ShortestPath/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex03.ShortestPath
+{
+    public class Program
+    {
+        // It is not always the shortest
+        // Must be optimized
+
+        private static List<List<int>> nodes = new List<List<int>>();
+        private static bool conatinsNode;
+        private static int index;
+        private static int x = -1;
+        private static bool stop = false;
+        static void Main()
+        {
+            int numberOfNodes = int.Parse(Console.ReadLine());
+            int numberofEdges = int.Parse(Console.ReadLine());
+
+            List<int> result = new List<int>();
+            int count = 0;
+
+            for (int i = 0; i < numberofEdges; i++)
+            {
+                List<int> node = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+                nodes.Add(node);
+            }
+
+            int startNodes = int.Parse(Console.ReadLine());
+            int endNode = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                result.Clear();
+                index = 0;
+                x=0;
+                BFS(startNodes, endNode, result, count);
+                if (stop)
+                {
+                    i = nodes.Count;
+                }
+            }
+        }
+        private static void BFS(int startNodes, int endNode,
+            List<int> result, int count)
+        {
+            x = 0;
+            foreach (var node in nodes)
+            {
+                x++;
+                if (node[0] == startNodes)
+                {
+                    if (!result.Contains(node[0]))
+                    {
+                        result.Add(node[0]);
+                    }
+                    result.Add(node[1]);
+
+                    startNodes = node[1];
+                    index = x - 1;
+                    count++;
+                    conatinsNode = true;
+
+                    if (result.Count > 0 && result[result.Count - 1] == endNode)
+                    {
+                        Console.WriteLine($"Shortest path length is: {count}");
+                        Console.WriteLine(String.Join(" ",result));
+                        stop = true;
+                        return;
+                    }
+
+                    break;
+                }
+
+                conatinsNode = false;
+            }
+
+            if (!conatinsNode)
+            {
+                nodes.RemoveAt(index);
+                return;
+            }
+
+            BFS(startNodes, endNode, result, count);
+        }
+    }
+}
