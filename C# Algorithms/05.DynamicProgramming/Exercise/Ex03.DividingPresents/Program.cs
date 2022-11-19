43db250e26b45c4795bbbd7234be6a042ef7e914
Nikzr0 <90499910+Nikzr0@users.noreply.github.com>
2022-11-19 11:48:06 +0200

    Data Introduction

---
+++ C# Algorithms/05.DynamicProgramming/Exercise/Ex03.DividingPresents/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex03.DividingPresents
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<int> Alan = new List<int>();
+            List<int> Bob = new List<int>();
+
+            int[] presents = Console.ReadLine().Split(" ").Select(int.Parse).OrderBy(x => x).ToArray();
+            int targetSum = presents.Sum() / 2;
+            Devide(presents, targetSum, Alan, Bob);
+        }
+        private static void Devide(int[] presents, int targetSum, List<int> alon, List<int> bob)
+        {
+            if (presents.Sum() == targetSum * 2)
+            {
+
+            }
+        }
+    }
+}
