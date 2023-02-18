d5674c11b0cfbd4eb718e5d8f79000a02f20af99
Nikzr0 <90499910+Nikzr0@users.noreply.github.com>
2023-02-18 11:53:33 +0200

    Cookies introduction

---
+++ C# Web/03.StateManagment/ExercisesWithStateManagement/ParallelExecutionExercises.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesWithStateManagement
+{
+    public class ParallelExecutionExercises
+    {
+        static int count;
+        static object lockObject = new object();
+        //Should be renamed before execution
+        static void RenameToMainBeforeExecution()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            Console.WriteLine(count);
+            PrintPrime(0, 10_000_000);
+            Console.WriteLine(count);
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        static void PrintPrime(int min, int max)
+        {
+            Parallel.For(min, max + 1, i =>
+            {
+                bool isPrime = true; 
+                for (int J = 2; J < Math.Sqrt(i); J++)
+                {
+                    if (i % J == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    lock (lockObject)
+                    {
+                        count++;
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ C# Web/03.StateManagment/ExercisesWithStateManagement/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesWithStateManagement
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            // Web page socket address  
+            TcpListener tcpListener = new TcpListener(
+                IPAddress.Loopback, 400);
+
+            // start
+            tcpListener.Start();
+
+            // The best way to write new line in http
+            const string NewLine = "\r\n";
+
+            while (true)
+            {
+                // Client implementation
+                var client = tcpListener.AcceptTcpClient();
+                using (var stream = client.GetStream())
+                {
+                    byte[] buffer = new byte[1000000000];
+                    var length = stream.Read(buffer, 0, buffer.Length);
+
+                    string requestStr = Encoding.UTF8.GetString(buffer, 0, length);
+                    Console.WriteLine(requestStr);
+
+                    string html = $"<h1>Welcome on my web page at{DateTime.Now}!</h1>";
+                    string html2 = $"<h1>I hope you enjoy it <3</h1>";
+                    string paragraph = $"<p>My name is Nikola and I am here to learn and have fun</p>";
+
+                    string response = "HTTP/1.1 200 OK" + NewLine +
+                        "Server: NikolaServer 2023" + NewLine +
+                        "Content-Type: text/html; charset=utf-8" + NewLine +
+                        "Content-Type: " + NewLine +
+                        "Set-Cookie: sid=firstCookieEver; Security; HttpOnly; Parh=/;Max-Age = " + (10) + NewLine +
+                        "Content-Lenght: " + (html.Length + html2.Length + paragraph.Length + NewLine +
+                        NewLine +
+                        html + html2 + paragraph + NewLine);
+
+                    byte[] responseArray = Encoding.UTF8.GetBytes(response);
+                    stream.Write(responseArray);
+
+                    Console.WriteLine(new string('-', 50));
+                }
+            }
+        }
+    }
+}
